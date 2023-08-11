18acb513f31d9f6a990805c67c5010bdee1f1a80
Jessey9 <sundayoj@yahoo.com>
2023-08-11 16:28:11 -0700

    Shell programming for C Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/***
+ * main - A C programm on hello world using the function printf
+ * Return: 0 (positive)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art," / n);
+return (0);
+}
