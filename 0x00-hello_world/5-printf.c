a2b1cd17fb06ec07f5d1c68834ab81758198c149
Jessey9 <sundayoj@yahoo.com>
2023-08-11 18:29:43 -0700

    C programme Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /***
- * main- C programme using the function printf and returns to 0 if successful
+ * this is the main function where the programme uses the function printf
+ * it returns to 0 if successful
  * Return: 0
  */
 int main(void)
