d8e496637f7711673bbb5df8fcc8a515266b75a1
Jessey9 <sundayoj@yahoo.com>
2023-08-11 19:27:03 -0700

    C programme Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,11 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	puts("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	puts("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	puts("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	puts("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	puts("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	puts("Size of a char: %d byte(s)\n", sizeof(char));
+	puts("Size of an int: %d byte(s)\n", sizeof(int));
+	puts("Size of a long int: %d byte(s)\n", (unsigned long) sizeof(long int));
+	puts("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	puts("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
 
