f26fe198733e3eb4df7fd3aea449785c39210a91
Jessey9 <sundayoj@yahoo.com>
2023-08-14 18:29:23 -0700

    program print in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Everytime 0 (Successful)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
