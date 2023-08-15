4afde65a7a86d23276fcf570c816fa9715c56f0c
Jessey9 <sundayoj@yahoo.com>
2023-08-14 17:19:57 -0700

    C programe variable else if and while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: 0 (Success)
@@ -8,16 +9,23 @@
 int main(void)
 {
 	int n;
+	int o;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	z = n % 10;
-	if (z > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, z);
-	if (z == 0)
-		printf("Last digit of %d is %d and is 0\n", n, z);
-	if printf(z < 6 && z !=0)
-		printf("Last digit of %d is %d and is less than 6 and is not 0 \n", n, z);
-	return (0)
+	/* your code goes there */
+	o = n % 10;
+	if (o > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, o);
+	}
+	else if (o < 6 && o != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, o);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is 0\n", n, o);
+	}
+	return (0);
 }
-
