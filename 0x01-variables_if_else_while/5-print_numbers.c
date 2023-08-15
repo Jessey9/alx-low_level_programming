33b7cd5697bed3a8b134d01617584c93d2b12d3d
Jessey9 <sundayoj@yahoo.com>
2023-08-14 19:07:17 -0700

    program print in numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Everytime 0 (Successful)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
