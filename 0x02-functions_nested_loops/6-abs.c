e7118c5127f2b9f88748c63807bb1c99e44a3afb
Jessey9 <sundayoj@yahoo.com>
2023-08-16 13:21:15 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * absolute value - computes the absolute value of an integer
+ * @c: The character to be checked
+ * Return: Everytime 0
+ */
+
+int abs(int j);
+{
+	int j;
+
+	if (
