ecd871b0433c341f145310e4ea8a104a950a15bf
Jessey9 <sundayoj@yahoo.com>
2023-08-16 05:37:10 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,15 @@
  * main - Entry point
  * Return: Everytime 0
  */
+
 void print_alphabet(void)
 {
 	char lc;
 
-	for (lc = 'a'; lc<= 'z'; lc++)
+	for (lc = 'a'; lc <= 'z'; lc++)
 	{
 	_putchar(lc);
 	}
-		
+
 	_putchar('\n');
 }
