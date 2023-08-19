afe7ebbffe969d22f3e034d317aa61e9fcd96208
Jessey9 <sundayoj@yahoo.com>
2023-08-18 21:19:54 -0700

    more function and nested loops task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,10 +3,10 @@
 /**
  * _isdigit - Checking to be sure a character is a digit
  * @x: The number to be checked
- * Retuen: 1 for a character taht will be a digit or else 0
+ * Retuen: 1 for a character that will be a digit or else 0
  */
 
-int +isdigit(int x)
+int _isdigit(int x)
 {
 	if (x >= 48 && x <= 57)
 	{
