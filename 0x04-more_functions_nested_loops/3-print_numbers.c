f27617bec718d02c20bc70422daa29fc8ee8dc14
Jessey9 <sundayoj@yahoo.com>
2023-08-18 21:31:44 -0700

    more function and nested loops task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - Print numbers from 0 to 9
+ * Return: The numbers since 0 up to 9
+ */
+
+voic print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
