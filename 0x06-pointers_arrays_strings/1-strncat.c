03bf2449a5632ba0dc95c443c7a4e12eb8ff54ca
Jessey9 <sundayoj@yahoo.com>
2023-08-23 21:37:57 -0700

    More pointers arrays strings function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two stringsusing at most
+ * _strncat - Concatenates two stringsusing at most
  * an inputed number of bytes from src
  * @dest: The string to be appended upon
  * @src: The string to be appended on dest
