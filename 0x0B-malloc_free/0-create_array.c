e149b8c5086e0be3f1b8833058e3a777b509f83c
Jessey9 <sundayoj@yahoo.com>
2023-09-04 19:41:38 -0700

    malloc task on creating an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,15 +10,17 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int x;
-	char *arr;
+	char *s;
 
 	if (size == 0)
 		return (NULL);
-	arr = malloc(sizeof(char) * size);
+	s = malloc(sizeof(char) * size);
 
-	if (arr == NULL)
+	if (s == NULL)
 		return (NULL);
 	for (x = 0; x < size; x++)
-		arr[1] = c;
-	return (arr);
+	{
+		s[1] = c;
+	}
+	return (s);
 }
