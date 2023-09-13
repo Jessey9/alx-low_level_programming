6a1f59b61e2323c4917702a898f24799a728e233
Jessey9 <sundayoj@yahoo.com>
2023-09-13 06:43:20 -0700

    Structure, typedef projects readme and task 0 for new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type structure of dog
+ * @name: Name of dog
+ * @age: Dog's age
+ * @owner: Name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
